229e924d8672f22ca52c8a6e3bff8076b00ecc3a
Synzenn <velriel42@gmail.com>
2025-12-19 21:06:58 +0700

    membuat dan menambahkan DestinasiHome.kt

---
+++ app/src/main/java/com/example/mysql19des/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.mysql19des.uicontroller.route
+
+import com.example.mysql19des.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.nama
+}

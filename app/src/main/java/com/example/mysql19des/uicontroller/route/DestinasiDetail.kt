ce3edac414ff0a81df36daa6e3dbbce7d6c51fe6
Synzenn <velriel42@gmail.com>
2025-12-19 21:07:11 +0700

    membuat dan menambahkan DestinasiDetail.kt

---
+++ app/src/main/java/com/example/mysql19des/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,8 @@
+package com.example.mysql19des.uicontroller.route
+
+import com.example.mysql19des.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+}

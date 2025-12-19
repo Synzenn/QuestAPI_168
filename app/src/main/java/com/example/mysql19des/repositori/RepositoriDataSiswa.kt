0e3bbeae027fec1f2ad52f6da3b7e3660a0297af
Synzenn <velriel42@gmail.com>
2025-12-19 21:05:36 +0700

    membuat dan menambahkan Repositori

---
+++ app/src/main/java/com/example/mysql19des/repositori/RepositoriDataSiswa.kt
@@ -0,0 +1,19 @@
+package com.example.mysql19des.repositori
+
+
+import com.example.mysql19des.apiservice.ServiceApiSiswa
+import com.example.mysql19des.modeldata.DataSiswa
+
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}

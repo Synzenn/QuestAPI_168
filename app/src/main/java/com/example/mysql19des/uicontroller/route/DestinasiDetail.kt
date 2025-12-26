e5a3c8bec3c25e83698476971356e6d09dd8dd67
Synzenn <velriel42@gmail.com>
2025-12-26 19:35:23 +0700

    menambahkan kodingan pada DestinasiDetail.kt

--- app/src/main/java/com/example/mysql19des/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/mysql19des/uicontroller/route/DestinasiDetail.kt
@@ -5,4 +5,7 @@ import com.example.mysql19des.R
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "item_detail"
     override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }

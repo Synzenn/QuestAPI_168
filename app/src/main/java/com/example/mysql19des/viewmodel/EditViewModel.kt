ffa1622ea56c06e1bea5dc1f9f6b4916e93b33fa
Synzenn <velriel42@gmail.com>
2025-12-26 19:37:32 +0700

    mmenambahkan kode untuk EditViewModel.kt

--- app/src/main/java/com/example/mysql19des/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/mysql19des/viewmodel/EditViewModel.kt
@@ -14,3 +14,46 @@ import com.example.mysql19des.repositori.RepositoryDataSiswa
 import com.example.mysql19des.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
 import retrofit2.Response
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa
+                .getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            val call: Response<Void> =
+                repositoryDataSiswa.editSatuSiswa(
+                    idSiswa,
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+
+            if (call.isSuccessful) {
+                println("Update Sukses : ${call.message()}")
+            } else {
+                println("Update ERROR : ${call.errorBody()}")
+            }
+        }
+    }
+}

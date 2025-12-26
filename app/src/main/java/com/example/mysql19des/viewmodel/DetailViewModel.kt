d490850e4a9f0eda3e0ad8722e4e6c671eef4139
Synzenn <velriel42@gmail.com>
2025-12-26 19:36:15 +0700

    menambahkan LIBRARY YANG DIBUTUHKAN

--- app/src/main/java/com/example/mysql19des/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/mysql19des/viewmodel/DetailViewModel.kt
@@ -1,2 +1,16 @@
 package com.example.mysql19des.viewmodel
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mysql19des.modeldata.DataSiswa
+import com.example.mysql19des.repositori.RepositoryDataSiswa
+import com.example.mysql19des.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+import retrofit2.Response

78b85d9a11c545738eb29c2a2a0202bc44fc0e68
Synzenn <velriel42@gmail.com>
2025-12-26 19:38:00 +0700

    menambahkan library yang dibutuhkan

---
+++ app/src/main/java/com/example/mysql19des/view/HalamanEdit.kt
@@ -0,0 +1,15 @@
+package com.example.mysql19des.view
+
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.mysql19des.viewmodel.EditViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mysql19des.viewmodel.provider.PenyediaViewModel
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.layout.padding
+import com.example.mysql19des.uicontroller.route.DestinasiEdit

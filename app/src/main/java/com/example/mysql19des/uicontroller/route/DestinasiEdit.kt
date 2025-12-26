cdfa7dd792f194172774bbd84636d7f920292ecc
Synzenn <velriel42@gmail.com>
2025-12-26 19:42:29 +0700

    membuat object, dan menambahakn route, title, itemid

--- app/src/main/java/com/example/mysql19des/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/mysql19des/uicontroller/route/DestinasiEdit.kt
@@ -1,3 +1,12 @@
 package com.example.mysql19des.uicontroller.route
 
 import com.example.mysql19des.R
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+
+}

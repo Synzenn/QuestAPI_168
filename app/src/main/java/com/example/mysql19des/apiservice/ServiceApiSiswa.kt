b61ba73a90a8f38fbd60d0c3ab6fc197b63bf96d
Synzenn <velriel42@gmail.com>
2025-12-26 19:32:04 +0700

    menambahkan put untuk editTM

--- app/src/main/java/com/example/mysql19des/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/mysql19des/apiservice/ServiceApiSiswa.kt
@@ -20,4 +20,7 @@ interface ServiceApiSiswa {
     @GET("baca1teman.php/{id}")
     suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
 }
